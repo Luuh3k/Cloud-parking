ef4ccf66f5cac47552ef75ca08d119104d3ead9f
luuh3k <lucas.digital18@gmail.com>
2022-11-04 13:22:39 -0300

    Criação do primeiro endpoint

--- src/main/java/desafiobootcamp/cloudparking/controller/HelloController.java
+++ src/main/java/desafiobootcamp/cloudparking/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class HelloController {
-test
+
     @GetMapping
     public String hello(){
         return "hello Dio. isso vai me dar uma vaga";
